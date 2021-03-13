14b8ff7d5416d4014aeb53c9c56e61d051bf0f27
naguiluz <80374335+naguiluz@users.noreply.github.com>
2021-03-12 22:57:32 -0600

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,8 @@
+for index in range(307):
+    if (index % 2) == 0:
+        print("loves me")
+    else:
+        print("loves me not")
+#for every index in the range of 0-307
+#if the index divided by two has a remainder of 0, print "loves me" (is even)
+#else print "loves me not"
