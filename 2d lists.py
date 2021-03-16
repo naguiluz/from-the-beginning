65905669e01e1d0e23a598dc961ddd12cd093eed
naguiluz <80374335+naguiluz@users.noreply.github.com>
2021-03-15 21:18:59 -0500

    Add files via upload

---
+++ 2d lists.py
@@ -0,0 +1,10 @@
+number_grid = [ #this list is made of several other lists. it allows us to create subsets of data within a list, and then access specific pieces inside
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+
+print(number_grid[0][0]) #the first number specifies row and the second column
+
